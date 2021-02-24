5ababfb43e25d364aed569c79d92975f78530f5f
JimeBB <jimeberbau@outlook.es>
2021-02-23 20:51:45 -0600

    migración comentarios y reseñas_comentarios

---
+++ database/migrations/2021_02_24_024625_resenas_comentarios.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ResenasComentarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resenas_comentarios', function (Blueprint $table) {
+
+            $table->integer('comentarios_id')->unsigned();
+        
+            $table->integer('resenas_id')->unsigned();
+        
+            $table->foreign('comentarios_id')->references('id')->on('comentarios')
+        
+                ->onDelete('cascade');
+        
+            $table->foreign('resenas_id')->references('id')->on('resenas')
+        
+                ->onDelete('cascade');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resenas_comentarios');
+    }
+}
