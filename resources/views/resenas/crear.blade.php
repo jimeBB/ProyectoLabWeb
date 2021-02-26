0839ebeb30a0bbb36ef9f1c18c43954fd3c9899e
99carlos99 <A01339011@itesm.mx>
2021-02-26 01:44:26 -0600

    Cambios en diseño

--- resources/views/resenas/crear.blade.php
+++ resources/views/resenas/crear.blade.php
@@ -6,35 +6,35 @@
 <form action="{{ route('resenas.store') }}" method="POST">
     @csrf
     <div>
-        <label for="">Nombre</label>
+        <label for="">Autor</label>
         <input type="text" name="nombre">
-    </div>
+    </div><br>
     <div>
         <label for="">Título</label>
         <input type="text" name="titulo">
-    </div>
+    </div><br>
     <div>
         <label for="">Fecha</label>
         <input type="date" name="fecha_creacion">
-    </div>
+    </div><br>
     <div>
         <label for="">Categoría</label>
-        <input type="text" name="categoria">
-    </div>
+        <t type="text" name="categoria">
+    </div><br>
      <div>
-        <label for="">Texto</label>
+        <label for="">Texto</label><br>
         <input type="text" name="texto">
-    </div>
+    </div><br>
     <div>
         <label for="">Likes</label>
         <input type="text" name="likes">
-    </div>
+    </div><br>
     <div>
         <label for="">ID Usuario</label>
         <input type="text" name="usuario_id">
-    </div>
+    </div><br>
     <div>
         <input type="submit" value="Store">
-    </div>
+    </div><br>
 </form>
 @endsection
--- resources/views/resenas/show.blade.php
+++ resources/views/resenas/show.blade.php
@@ -1,31 +1,69 @@
 @extends('layouts.main')
 
 @section('content')
+<style>
+h1 {
+    font-family: arial, sans-serif;
+}
+p {
+    font-family: arial, sans-serif; 
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+  
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+.escribir:link, .escribir:visited {
+  border: solid 1px;
+  font-family: arial, sans-serif;
+  background-color: black;
+  color: white;
+  padding: 10px 15px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+.escribir:hover, .escribir:active {
+  background-color: white;
+  color: black;
+  border: solid 1px;
+}
+</style>
 <table>
     <thead>
         <tr>
             <th>Reseña</th>
-            <th>Nombre</th>
-            <th>Titulo</th>
+            <th>Autor</th>
             <th>Fecha</th>
             <th>Categoria</th>
-            <th>Texto</th>
             <th>Likes</th>
             <th>Usuario ID</th>
-            <th></th>
         </tr>
     </thead>
     <tbody>
         <tr>
             <td>{{ $resena->id }}</td>
             <td>{{ $resena->nombre }}</td>
-            <td>{{ $resena->titulo }}</td>
             <td>{{ $resena->fecha_creacion }}</td>
             <td>{{ $resena->categoria }}</td>
-            <td>{{ $resena->texto }}</td>
             <td>{{ $resena->likes}}</td>
             <td>{{ $resena->usuario_id}}</td>
         </tr>
     </tbody>
 </table>
+<h1>{{ $resena->titulo }}</h1>
+<p>{{ $resena->texto }} </p>
 @endsection
