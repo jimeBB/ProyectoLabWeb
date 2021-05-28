a0944e371a286fa7f727db25420cd6a8d38932c2
99carlos99 <A01339011@itesm.mx>
2021-05-28 16:17:54 -0500

    cambio reacciones index

--- resources/views/landingPage/guest.blade.php
+++ resources/views/landingPage/guest.blade.php
@@ -109,17 +109,17 @@
 
                                                             <ul class="review-share">
 
-                                                                <li class="readMore"><a href="{{ route('resena.show-user', ['resena' => $item]) }}" class="more-button ">
-                                                                        Leer más</a></li>
+                                                                <li style="cursor:pointer; margin-bottom:20px;"><a href="{{ route('resena.show-user', ['resena' => $item]) }}" class="more-button ">
+                                                                        Leer más</a></li><br><br>
 
-                                                                <li  ><svg  onclick="updateLikes({{ $item->id }},{{ $item->likes }})" onclick="callEvent()" xmlns="http://www.w3.org/2000/svg" width="16"
-                                                                        height="16" fill="currentColor"
+                                                                <li style="cursor:pointer; margin-left:-30px"><svg  onclick="updateLikes({{ $item->id }},{{ $item->likes }})" onclick="callEvent()" xmlns="http://www.w3.org/2000/svg" width="30"
+                                                                        height="30" fill="currentColor"
                                                                         class="bi bi-hand-thumbs-up-fill share-icon"
-                                                                        viewBox="0 0 16 16" >
+                                                                        viewBox="0 0 20 20" >
                                                                         <path
                                                                             d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.964.22.817.533 2.512.062 4.51a9.84 9.84 0 0 1 .443-.05c.713-.065 1.669-.072 2.516.21.518.173.994.68 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.162 3.162 0 0 1-.488.9c.054.153.076.313.076.465 0 .306-.089.626-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.826 4.826 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.616.849-.231 1.574-.786 2.132-1.41.56-.626.914-1.279 1.039-1.638.199-.575.356-1.54.428-2.59z" />
-                                                                             </svg><a class="likes-review" id="{{ $item->id }}_likes"> {{ $item->likes }}</a></li>
-                                                                <li><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                                            </svg><a class="likes-review" id="{{ $item->id }}_likes"> {{ $item->likes }}</a></li>
+                                                                <!--<li><svg xmlns="http://www.w3.org/2000/svg" width="16"
                                                                         height="16" fill="currentColor"
                                                                         class="bi bi-facebook share-icon" viewBox="0 0 16 16">
                                                                         <path
@@ -130,7 +130,7 @@
                                                                         class="bi bi-twitter share-icon" viewBox="0 0 16 16">
                                                                         <path
                                                                             d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                                                                    </svg></li>
+                                                                    </svg></li>-->
                                                             </ul>
                                                         </div>
                                                     </div>
